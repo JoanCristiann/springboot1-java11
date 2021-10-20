a76dc3e1444f2f501679c3c40f65b015cf605184
JoanCristiann <joancristiann@gmail.com>
2021-10-20 01:35:49 -0300

    Exception handling - update

---
+++ src/main/java/com/example/course/services/exceptions/EntityException.java
@@ -0,0 +1,10 @@
+package com.example.course.services.exceptions;
+
+public class EntityException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public EntityException(String msg) {
+		super(msg);
+	}
+
+}
